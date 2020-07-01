d75cdf438ff1a814bd4f58ed19e8ad484591e3d9
Ilan <ilan@tuqqi.com>
2020-07-01 22:02:52 +0300

    Updates

---
+++ precache-manifest.9e586fa4806119d61071e9d4dcec7fe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a369f68b91e668d6d75a28b3d313e6",
+    "url": "/herolo-ilan-khirin-30-06-2020/index.html"
+  },
+  {
+    "revision": "03b3df9a3c9b8040b571",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/2.215bf1c0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/2.215bf1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422d1dfc13f638ea5abd",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/main.8becb88a.chunk.js"
+  },
+  {
+    "revision": "91d4d343a77192ae4d2d",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/runtime-main.6317d756.js"
+  }
+]);

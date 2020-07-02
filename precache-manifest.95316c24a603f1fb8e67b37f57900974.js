430d0e9403fcce9d8153bb36f3d1f9ab668336c3
Ilan <ilan@tuqqi.com>
2020-07-02 10:47:41 +0300

    Updates

---
+++ precache-manifest.95316c24a603f1fb8e67b37f57900974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe34c70041055e8334b1fb94f07e106",
+    "url": "/herolo-ilan-khirin-30-06-2020/index.html"
+  },
+  {
+    "revision": "49b054e76cfcf96808fb",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/2.0b910632.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/2.0b910632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b814cc29c41bd7cec7",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/main.c6daa976.chunk.js"
+  },
+  {
+    "revision": "91d4d343a77192ae4d2d",
+    "url": "/herolo-ilan-khirin-30-06-2020/static/js/runtime-main.6317d756.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/herolo-ilan-khirin-30-06-2020/precache-manifest.fe8ec039e6d1c65196b2a7dffd8ba148.js"
+  "/herolo-ilan-khirin-30-06-2020/precache-manifest.95316c24a603f1fb8e67b37f57900974.js"
 );
 
 self.addEventListener('message', (event) => {
